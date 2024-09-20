8f0a1c9152d6dd9c1e26ae1c663b390182cb3521
bangchand <chandrakusumaa133@gmail.com>
2024-09-20 21:13:46 +0700

    menambahkan middleware auth untuk halaman chat

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('/u', [AllController::class, 'no_chat'])->name('home');
-Route::get('/u/chat/{uuid}', [AllController::class, 'chat'])->name('chat');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/u', [AllController::class, 'no_chat'])->name('home');
+    Route::get('/u/chat/{uuid}', [AllController::class, 'chat'])->name('chat');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
